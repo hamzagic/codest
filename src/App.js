aa4b2e8908baa64ac8b271b2dfe7927fb00f27ba
Francis Hamzagic <francishamzagic@gmail.com>
2022-05-25 00:35:58 -0300

    Adding in-line comments

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 
 import { addWatchedMovie, add, removeWatchedMovie, getWatchedMovies, getAllMovies } from './index.js';
 
+// You can create separate components for each part of the
+// application to make the code reusable and better maintainable
+// Style the application by importing css files to each component
 const getMoviesComponents = (movies) => {
   var components = [];
 
+  // you can make use of array.map to render this list
+  // so that you don't need to create a new variable (components)
+  // to render the list
   movies.forEach(function(movie) {
     components.push(
       <div className="all">
@@ -13,6 +19,7 @@ const getMoviesComponents = (movies) => {
           <img src={movie.image} height="100px" />
         </div>
         <span>
+           {/* You could use arrow function here as well */}
           <a className="movie-watched" href="#" onClick={function() { addWatchedMovie(movie.title, movie.comment, movie.image) }}>
             {movie.title}
           </a>
@@ -30,9 +37,13 @@ const getMoviesComponents = (movies) => {
   return components;
 }
 
+// You can create a separate component to handle the watched movies list
+// Make use of arrow functions to keep a code pattern
 function getWatchedMoviesComponents(movies) {
   var components = [];
-
+  // you can make use of array.map to render this list
+  // so that you don't need to create a new variable (components)
+  // to render the list
   movies.forEach(function(movie) {
     components.push(movie && (
       <div className="watched">
@@ -40,6 +51,7 @@ function getWatchedMoviesComponents(movies) {
           <img src={movie.image} height="100px" />
         </div>
         <span>
+          {/* Make use of arrow function here as well */}
           <a className="movie-watched" href="#" onClick={function() { removeWatchedMovie(movie.title) }}>
             {movie.title}
           </a>
@@ -62,9 +74,16 @@ function App(props) {
     <div className="App">
       <h1>Codest Movies!</h1>
       <h1>Add movie!</h1>
+      {/* Make use of useState hook to better handle variable values. 
+      You can also create handler functions to add some validation (e.g.)
+      empty fields */}
       <b>TITLE:<br /><input type="text" onChange={function(e) { title = e.target.value; }} /></b><br />
       <b>IMAGE URL:<br /><input type="text" onChange={function(e) { image = e.target.value; }} /></b><br />
       <b>COMMENT:<br /><input type="text" onChange={function(e) { comment = e.target.value; }} /></b><br />
+      {/* make sure to follow the arguments order in the add function
+      otherwise the data will not be saved correctly (comment is the second
+      argument, image is the third).
+      */}
       <input type="button" onClick={function(e) { add(title, image, comment); }} value="ADD MOVIE" />
 
       <h1>Watchlist:</h1>
--- src/index.js
+++ src/index.js
@@ -38,7 +38,9 @@ export function getAllMovies() {
 		return JSON.parse(movies);
 	}
 }
-
+// movie object doesn't have a key called 'description'
+// change to comment instead 
+// otherwise it won't be rendered in the movies list
 export function add(title, description, image) {
 	var movie = {};
 	movie.title = title;
@@ -52,7 +54,9 @@ export function add(title, description, image) {
 
 	render();
 }
-
+// movie object doesn't have a key called 'description'
+// change to comment instead
+// otherwise it won't be rendered in the movies list
 export function addWatchedMovie(title, description, image) {
 	var movie = {};
 	movie.title = title;
@@ -60,6 +64,7 @@ export function addWatchedMovie(title, description, image) {
 	movie.image = image;
 
 	var movies = getWatchedMovies();
+	// make sure to check if the movie is already on the list before pushing
 	movies.push(movie);
 
 	localStorage.setItem('movies-watched', JSON.stringify(movies));
@@ -73,6 +78,7 @@ export function removeWatchedMovie(title) {
 	for (var i = 0; i < movies.length; i++) {
 	   if (!movies[i]) continue;
 		if (movies[i].title == title) {
+			// remove the movie item instead of creating a null variable
 			movies[i] = null
 		}
 	}
